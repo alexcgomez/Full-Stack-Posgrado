eaff6d87f1498ebcbf101ddd092be69d53ce93eb
Alex <alexcaceres93@gmail.com>
2020-03-18 18:06:57 +0100

    Añadido modules Javascript

---
+++ 5.- Javascript/Ejemplos/2020-03-17/commonjs2/index.js
@@ -0,0 +1,4 @@
+
+const fs = require('fs');
+
+fs.writeFileSync('prueba.txt', 'Texto de prueba\n');
---
+++ 5.- Javascript/Ejemplos/2020-03-17/es6_4/index.mjs
@@ -0,0 +1,7 @@
+
+import Circle, { area, circumference } from './circle.mjs';
+
+const c = new Circle(10);
+console.log(c);
+console.log(area(c));
+console.log(circumference(c));
